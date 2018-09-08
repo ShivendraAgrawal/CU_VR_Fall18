7568713fe6ba64c111cbc3791eaa1df52f9ac748
ShivendraAgrawal <shivendra.agrawal@gmail.com>
2018-09-07 21:06:15 -0600

    Add assignment 1

---
+++ Assignment 1/HelloCardboard/Temp/gradleOut/build/generated/source/buildConfig/release/com/shivendraagrawal/helloball/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.shivendraagrawal.helloball;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.shivendraagrawal.helloball";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
